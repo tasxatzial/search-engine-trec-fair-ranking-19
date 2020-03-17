cb9499d053806d54917a1b8101c79062b17e8381
Panagiotis Papadakos <papadako@ics.forth.gr>
2020-03-17 20:38:34 +0200

    [Config] Class that holds configuration options as given in the themis.config file

---
+++ src/main/java/gr/csd/uoc/hy463/themis/config/Config.java
@@ -0,0 +1,67 @@
+/*
+ * themis - A fair search engine for scientific articles
+ *
+ * Computer Science Department
+ *
+ * University of Crete
+ *
+ * http://www.csd.uoc.gr
+ *
+ * Project for hy463 Information Retrieval Systems course
+ * Spring Semester 2020
+ *
+ * LICENCE: TO BE ADDED
+ *
+ * Copyright 2020
+ *
+ */
+package gr.csd.uoc.hy463.themis.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Class that holds configuration options for the themis system
+ *
+ * @author Panagiotis Papadakos (papadako@ics.forth.gr)
+ */
+public class Config {
+
+    private InputStream input;
+    private static final Logger LOGGER = LogManager.getLogger(Config.class);
+    private Properties prop;
+
+    public Config() throws IOException {
+        prop = new Properties();
+        String propFileName = "/themis.config";
+
+        input = getClass().getResourceAsStream(propFileName);
+
+        if (input != null) {
+            prop.load(input);
+        } else {
+            LOGGER.error("property file '" + propFileName + "' not found in the classpath");
+        }
+    }
+
+    /**
+     * Check if config is valid
+     *
+     * @return
+     */
+    public boolean valid() {
+        return input != null;
+    }
+
+    /**
+     * Currently we only have a property about the index path
+     *
+     * @return
+     */
+    public String getIndexPath() {
+        return prop.getProperty("INDEX_PATH");
+    }
+}
