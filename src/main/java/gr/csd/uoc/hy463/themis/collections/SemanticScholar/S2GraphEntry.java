0731d92445d566dedad28badd21dab1b96164fce
Panagiotis Papadakos <papadako@ics.forth.gr>
2020-03-21 21:51:37 +0200

    [S2GraphEntry] Will be used in the future for link-based analysis

---
+++ src/main/java/gr/csd/uoc/hy463/themis/collections/SemanticScholar/S2GraphEntry.java
@@ -0,0 +1,35 @@
+/*
+ * themis - A fair search engine for scientific articles
+ *
+ * Currently over the Semantic Scholar Open Research Corpus
+ * http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/corpus/
+ *
+ * Collaborative work with the undergraduate/graduate students of
+ * Information Retrieval Systems (hy463) course
+ * Spring Semester 2020
+ *
+ * -- Writing code during COVID-19 pandemic times :-( --
+ *
+ * Aiming to participate in TREC 2020 Fair Ranking Track
+ * https://fair-trec.github.io/
+ *
+ * Computer Science Department http://www.csd.uoc.gr
+ * University of Crete
+ * Greece
+ *
+ * LICENCE: TO BE ADDED
+ *
+ * Copyright 2020
+ *
+ */
+package gr.csd.uoc.hy463.themis.collections.SemanticScholar;
+
+/**
+ * This class will be used in the future to hold information related to link
+ * based analysis (citations, authors)
+ *
+ * @author Panagiotis Papadakos <papadako at ics.forth.gr>
+ */
+public class S2GraphEntry {
+
+}
