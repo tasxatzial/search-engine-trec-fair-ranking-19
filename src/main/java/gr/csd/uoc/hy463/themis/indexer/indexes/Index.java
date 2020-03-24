67541ae81ac4fd7396291bc6b2ff8ef111434202
Panagiotis Papadakos <papadako@ics.forth.gr>
2020-03-24 20:30:20 +0200

    [Comment] It is best to hold the document id also in the posting file

--- src/main/java/gr/csd/uoc/hy463/themis/indexer/indexes/Index.java
+++ src/main/java/gr/csd/uoc/hy463/themis/indexer/indexes/Index.java
@@ -98,13 +98,13 @@ public class Index {
      * =========================================================================
      * 3) DOCUMENTS FILE => documents.idx (Random Access File)
      *
-     * For each entry it stores: | Title (variable bytes / UTF-8) |
-     * Author_1,Author_2, ...,Author_k (variable bytes / UTF-8) | AuthorID_1,
-     * AuthorID_2, ...,Author_ID_k (variable size /ASCII) | Year (short => 2
-     * bytes)| Journal Name (variable bytes / UTF-8) | The weight (norm) of
-     * Document (double => 8 bytes)| Length of Document (int => 4 bytes) |
-     * PageRank Score (double => 8 bytes => this will be used in the second
-     * phase of the project)
+     * For each entry it stores: | DOCUMENT_ID (40 ASCII chars => 40 bytes) |
+     * Title (variable bytes / UTF-8) | Author_1,Author_2, ...,Author_k
+     * (variable bytes / UTF-8) | AuthorID_1, AuthorID_2, ...,Author_ID_k
+     * (variable size /ASCII) | Year (short => 2 bytes)| Journal Name (variable
+     * bytes / UTF-8) | The weight (norm) of Document (double => 8 bytes)|
+     * Length of Document (int => 4 bytes) | PageRank Score (double => 8 bytes
+     * => this will be used in the second phase of the project)
      *
      * ==> IMPORTANT NOTES
      *
