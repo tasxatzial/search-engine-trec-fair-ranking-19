e4249d20b042373d6fc725fb1b6ddda11fa274fc
Panagiotis Papadakos <papadako@ics.forth.gr>
2020-03-31 18:03:05 +0300

    [S2JsonEntryReader] Explicit check of values

--- src/main/java/gr/csd/uoc/hy463/themis/lexicalAnalysis/collections/SemanticScholar/S2JsonEntryReader.java
+++ src/main/java/gr/csd/uoc/hy463/themis/lexicalAnalysis/collections/SemanticScholar/S2JsonEntryReader.java
@@ -59,11 +59,13 @@ public class S2JsonEntryReader {
             entry.setId(id);
 
             // Get the title for example
-            String title = (String) jsonObject.get("title");
+            String titleCheck = (String) jsonObject.get("title");
+            String title = titleCheck != null ? titleCheck : "";
             entry.setTitle(title);
 
             // Get abstract for example
-            String paperAbstract = (String) jsonObject.get("paperAbstract");
+            String paperAbstractCheck = (String) jsonObject.get("paperAbstract");
+            String paperAbstract = paperAbstractCheck != null ? paperAbstractCheck : "";
             entry.setPaperAbstract(paperAbstract);
 
             // Read entities. A JSONArray
@@ -101,7 +103,8 @@ public class S2JsonEntryReader {
             entry.setAuthors(authors);
 
             // Get journal for example
-            String journal = (String) jsonObject.get("journalName");
+            String journalCheck = (String) jsonObject.get("journalName");
+            String journal = journalCheck != null ? journalCheck : "";
             entry.setJournalName(journal);
 
             // Read sources. A JSONArray
@@ -113,11 +116,13 @@ public class S2JsonEntryReader {
             entry.setSources(sources);
 
             // Get year for example
-            int year = ((Long) jsonObject.get("year")).intValue();
+            Long yearLong = (Long) jsonObject.get("year");
+            int year = yearLong != null ? yearLong.intValue() : 0;
             entry.setYear(year);
 
             // Get venue for example
-            String venue = (String) jsonObject.get("venue");
+            String venueCheck = (String) jsonObject.get("venue");
+            String venue = venueCheck != null ? venueCheck : "";
             entry.setVenue(venue);
 
         } catch (ParseException e) {
@@ -129,52 +134,35 @@ public class S2JsonEntryReader {
 
     public static void main(String[] args) {
         String json = "{\n"
-                + "  \"id\": \"4cd223df721b722b1c40689caa52932a41fcc223\",\n"
-                + "  \"title\": \"Knowledge-rich, computer-assisted composition of Chinese couplets\",\n"
-                + "  \"paperAbstract\": \"Recent research effort in poem composition has focused on the use of\n"
-                + "   automatic language generation...\",\n"
-                + "  \"entities\": [\n"
-                + "    \"Conformance testing\",\n"
-                + "    \"Natural language generation\",\n"
-                + "    \"Natural language processing\",\n"
-                + "    \"Parallel computing\",\n"
-                + "    \"Stochastic grammar\",\n"
-                + "    \"Web application\"\n"
-                + "  ],\n"
-                + "  \"fieldsOfStudy\": [\n"
-                + "      \"Computer Science\"\n"
-                + "  ],\n"
-                + "  \"s2Url\": \"https://semanticscholar.org/paper/4cd223df721b722b1c40689caa52932a41fcc223\",\n"
-                + "  \"s2PdfUrl\": \"\",\n"
-                + "  \"pdfUrls\": [\n"
-                + "    \"https://doi.org/10.1093/llc/fqu052\"\n"
-                + "  ],\n"
-                + "  \"authors\": [\n"
-                + "    {\n"
-                + "      \"name\": \"John Lee\",\n"
-                + "      \"ids\": [\n"
-                + "        \"3362353\"\n"
-                + "      ]\n"
-                + "    }\n"
-                + "  ],\n"
-                + "  \"inCitations\": [\n"
-                + "    \"c789e333fdbb963883a0b5c96c648bf36b8cd242\"\n"
-                + "  ],\n"
-                + "  \"outCitations\": [\n"
-                + "    \"abe213ed63c426a089bdf4329597137751dbb3a0\",\n"
-                + "    \"...\"\n"
-                + "  ],\n"
-                + "  \"year\": 2016,\n"
-                + "  \"venue\": \"DSH\",\n"
-                + "  \"journalName\": \"DSH\",\n"
-                + "  \"journalVolume\": \"31\",\n"
-                + "  \"journalPages\": \"152-163\",\n"
-                + "  \"sources\": [\n"
-                + "    \"DBLP\"\n"
-                + "  ],\n"
-                + "  \"doi\": \"10.1093/llc/fqu052\",\n"
-                + "  \"doiUrl\": \"https://doi.org/10.1093/llc/fqu052\",\n"
-                + "  \"pmid\": \"\"\n"
+                + "	\"entities\": [],\n"
+                + "	\"journalVolume\": \"\",\n"
+                + "	\"journalPages\": \"\",\n"
+                + "	\"pmid\": \"\",\n"
+                + "	\"fieldsOfStudy\": [\"Physics\"],\n"
+                + "	\"year\": 2015,\n"
+                + "	\"outCitations\": [\"2497ed63572e8d5e5fe7945f0b23e0d090acd51c\", \"03b317054274da28acfb2c8e082f38d7dcfdce04\", \"070c58ff3d4f5ca3383c20a23af3594ae6e564ab\", \"f9a1951720cafa3706b341c0d14ddd57d9c83043\", \"26052227014c270c3f6013d98fdb8db1b80f8607\", \"8de63b8021633e45585874468ff5fe4bfe3ee476\", \"91d9b8d56ce67a90abfe0c9fc7483b8220ad3c66\", \"5baead167bceac9bdcbd7ac808620bb8987da323\", \"778f2e33cb7b0dfc0d3925df852fa4e576e75890\", \"88a11402d59f026ae5cd93f044e0c038f4373d51\", \"c2038b5d11a4dd9017d7a410b93f088f4dc8d1e4\", \"fa1aff91383e227fc115fc0621fd7452ebca46ab\"],\n"
+                + "	\"s2Url\": \"https://semanticscholar.org/paper/1b2f4e5be76a0a746b72110b447b42fffa046b5c\",\n"
+                + "	\"s2PdfUrl\": \"\",\n"
+                + "	\"id\": \"1b2f4e5be76a0a746b72110b447b42fffa046b5c\",\n"
+                + "	\"authors\": [{\n"
+                + "		\"name\": \"Xiang Fa Liu\",\n"
+                + "		\"ids\": [\"153201706\"]\n"
+                + "	}, {\n"
+                + "		\"name\": \"Guodong Xia\",\n"
+                + "		\"ids\": [\"46932503\"]\n"
+                + "	}, {\n"
+                + "		\"name\": \"Guo-zhen Yang\",\n"
+                + "		\"ids\": [\"50147063\"]\n"
+                + "	}],\n"
+                + "	\"journalName\": \"\",\n"
+                + "	\"paperAbstract\": \"Abstract Experimental investigations on the characteristics of air–water two-phase flow in the vertical helical rectangular channel are performed using the high speed flow visualization. The flow pattern map and the transition in the helical rectangular channel are presented. The flow pattern evolution in different positions of the helical rectangular channel is illustrated. The discussion on the coalescence of the bubble and slug is presented. The slug velocity, slug length distribution, liquid slug frequency, falling liquid film velocity and falling film thickness along the slug are investigated. The dimensionless liquid film thickness of the annular flow on the outer side of the channel is measured using the digital image processing algorithm.\",\n"
+                + "	\"inCitations\": [],\n"
+                + "	\"pdfUrls\": [\"http://www.mechwork.ir/uploads/papers/4-Experimental%20study%20on%20the%20characteristics%20of%20air-water%20two-phase%20flow%20in%20vertical.pdf\"],\n"
+                + "	\"title\": \"Experimental study on the characteristics of air–water two-phase flow in vertical helical rectangular channel\",\n"
+                + "	\"doi\": \"10.1016/j.ijmultiphaseflow.2015.03.012\",\n"
+                + "	\"sources\": [],\n"
+                + "	\"doiUrl\": \"https://doi.org/10.1016/j.ijmultiphaseflow.2015.03.012\",\n"
+                + "	\"venue\": \"\"\n"
                 + "}";
 
         System.out.println(json);
