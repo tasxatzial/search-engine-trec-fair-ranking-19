a075eea28749b2eadf461ef5baa906de40320852
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-04-04 12:06:35 +0300

    add a basic GUI

--- src/main/java/gr/csd/uoc/hy463/themis/Themis.java
+++ src/main/java/gr/csd/uoc/hy463/themis/Themis.java
@@ -2,13 +2,18 @@ package gr.csd.uoc.hy463.themis;
 
 import gr.csd.uoc.hy463.themis.ui.CreateIndex;
 import gr.csd.uoc.hy463.themis.ui.Search;
+import gr.csd.uoc.hy463.themis.ui.View;
 
 public class Themis {
     public static CreateIndex createIndex;
     public static Search search;
+    public static View view;
 
     public static void main(String[] args) {
         createIndex = new CreateIndex();
         createIndex.create();
+
+        view = new View();
+        view.setVisible(true);
     }
 }
---
+++ src/main/java/gr/csd/uoc/hy463/themis/ui/View.java
@@ -0,0 +1,48 @@
+package gr.csd.uoc.hy463.themis.ui;
+
+import javax.swing.*;
+
+public class View extends JFrame {
+
+    /* The main menu bar */
+    private JMenuBar _menu;
+
+    /* The "create index" menu item */
+    private JMenuItem _createIndex;
+
+    /* The "load index" menu item */
+    private JMenuItem _loadIndex;
+
+    /* The "query collection" menu item */
+    private JMenuItem _queryCollection;
+
+    public View() {
+        initMenu();
+        pack();
+        setTitle("Themis search engine v0.1");
+        setSize(800, 600); //initial frame size
+    }
+
+    /* Initializes the menu bar */
+    private void initMenu() {
+
+        /* index menu */
+        JMenu index = new JMenu("Index");
+        _createIndex = new JMenuItem("Create index");
+        _loadIndex = new JMenuItem("Load index");
+        index.add(_createIndex);
+        index.add(_loadIndex);
+
+        /* search menu */
+        JMenu search = new JMenu("Search");
+        _queryCollection = new JMenuItem("Query collection");
+        search.add(_queryCollection);
+
+        /* main menu bar */
+        _menu = new JMenuBar();
+        _menu.add(index);
+        _menu.add(search);
+
+        setJMenuBar(_menu);
+    }
+}
