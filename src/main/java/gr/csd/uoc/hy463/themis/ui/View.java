19b489c79b165e9113b23d83ceb0fc4643ce50d8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-04-05 09:42:10 +0300

    add a GUI title area

--- src/main/java/gr/csd/uoc/hy463/themis/ui/View.java
+++ src/main/java/gr/csd/uoc/hy463/themis/ui/View.java
@@ -1,6 +1,7 @@
 package gr.csd.uoc.hy463.themis.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class View extends JFrame {
 
@@ -16,11 +17,24 @@ public class View extends JFrame {
     /* The "query collection" menu item */
     private JMenuItem _queryCollection;
 
+    /* The font of any text except the menu/title items */
+    private Font _textFont;
+
+    /* Title area (top) */
+    private JLabel _titleArea;
+
+    /* Everything sits on top of this pane */
+    private JLayeredPane _mainPane;
+
     public View() {
         initMenu();
         pack();
         setTitle("Themis search engine v0.1");
         setSize(800, 600); //initial frame size
+        _textFont = new Font("SansSerif", Font.PLAIN, 16);
+        _mainPane = new JLayeredPane();
+        initTitleArea();
+        getContentPane().add(_mainPane);
     }
 
     /* Initializes the menu bar */
@@ -44,6 +58,28 @@ public class View extends JFrame {
         _menu.add(search);
 
         setJMenuBar(_menu);
+    }
+
+    /* Creates the title label */
+    private void initTitleArea() {
+        _titleArea = new JLabel("Themis");
+        _titleArea.setFont(new Font("SansSerif", Font.PLAIN, 24));
+        _titleArea.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        setTitleAreaBounds();
+        _mainPane.add(_titleArea);
+    }
+
+    /**
+     * Sets the proper bounds of the title area
+     */
+    public void setTitleAreaBounds() {
+        Dimension frameDim = getBounds().getSize();
+        int titleAreaWidth = frameDim.width - getInsets().left - getInsets().right;
+        int titleAreaHeight = 50;
+        int titleAreaX = 0;
+        int titleAreaY = 0;
+        _titleArea.setBounds(titleAreaX, titleAreaY, titleAreaWidth, titleAreaHeight);
+        _titleArea.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
     /**
