0af6748f9065f0ed00e1eca8744563f4d6b80d3c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-05-10 16:02:37 +0300

    use the program arguments to select between a GUI and a terminal based ui

--- src/main/java/gr/csd/uoc/hy463/themis/Themis.java
+++ src/main/java/gr/csd/uoc/hy463/themis/Themis.java
@@ -20,44 +20,51 @@ public class Themis {
     private static Search search;
     private static View view;
 
-    public static void main(String[] args) {
-        view = new View();
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0 && args[0].equals("swing_window")) {
+            view = new View();
 
-        /* close window button listener */
-        view.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                if ((createIndex != null && createIndex.isRunning() && !view.showYesNoMessage(createIndex.getTask())) ||
-                        (search != null && search.isRunning() && !view.showYesNoMessage(search.getTask()))) {
-                    view.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+            /* close window button listener */
+            view.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    if ((createIndex != null && createIndex.isRunning() && !view.showYesNoMessage(createIndex.getTask())) ||
+                            (search != null && search.isRunning() && !view.showYesNoMessage(search.getTask()))) {
+                        view.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+                    } else {
+                        view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                        System.exit(0);
+                    }
                 }
-                else {
-                    view.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-                    System.exit(0);
+            });
+
+            /* resized window listeners */
+            view.addComponentListener(new ComponentAdapter() {
+                public void componentResized(ComponentEvent e) {
+                    view.setIndexViewBounds();
+                    view.setTitleAreaBounds();
+                    view.setSearchViewBounds();
                 }
-            }
-        });
+            });
 
-        /* resized window listeners */
-        view.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent e) {
-                view.setIndexViewBounds();
-                view.setTitleAreaBounds();
-                view.setSearchViewBounds();
-            }
-        });
+            /* maximized window listeners */
+            view.addWindowStateListener(e -> view.setIndexViewBounds());
+            view.addWindowStateListener(e -> view.setTitleAreaBounds());
+            view.addWindowStateListener(e -> view.setSearchViewBounds());
 
-        /* maximized window listeners */
-        view.addWindowStateListener(e -> view.setIndexViewBounds());
-        view.addWindowStateListener(e -> view.setTitleAreaBounds());
-        view.addWindowStateListener(e -> view.setSearchViewBounds());
+            /* add a listeners on menu items */
+            view.get_createIndex().addActionListener(new createIndexListener());
+            view.get_queryCollection().addActionListener(new searchListener());
+            view.get_loadIndex().addActionListener(new loadIndexListener());
 
-        /* add a listeners on menu items */
-        view.get_createIndex().addActionListener(new createIndexListener());
-        view.get_queryCollection().addActionListener(new searchListener());
-        view.get_loadIndex().addActionListener(new loadIndexListener());
-
-        view.setVisible(true);
+            view.setVisible(true);
+        }
+        else {
+            //change the code here
+            createIndex = new CreateIndex();
+            createIndex.deleteIndex();
+            createIndex.createIndex();
+        }
     }
 
     /**
@@ -67,7 +74,7 @@ public class Themis {
     public static void print(String text) {
         try {
             if (view == null) {
-                System.out.println(text);
+                System.out.print(text);
             }
             else {
                 view.print(text);
