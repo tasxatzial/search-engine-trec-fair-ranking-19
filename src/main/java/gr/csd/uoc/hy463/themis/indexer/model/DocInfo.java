be7daec6d0d3d84c19faa46c558c13c4ae6e881e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-01-27 17:47:43 +0200

    remove obsolete comments

--- src/main/java/gr/csd/uoc/hy463/themis/indexer/model/DocInfo.java
+++ src/main/java/gr/csd/uoc/hy463/themis/indexer/model/DocInfo.java
@@ -1,27 +1,3 @@
-/*
- * themis - A fair search engine for scientific articles
- *
- * Currently over the Semantic Scholar Open Research Corpus
- * http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/corpus/
- *
- * Collaborative work with the undergraduate/graduate students of
- * Information Retrieval Systems (hy463) course
- * Spring Semester 2020
- *
- * -- Writing code during COVID-19 pandemic times :-( --
- *
- * Aiming to participate in TREC 2020 Fair Ranking Track
- * https://fair-trec.github.io/
- *
- * Computer Science Department http://www.csd.uoc.gr
- * University of Crete
- * Greece
- *
- * LICENCE: TO BE ADDED
- *
- * Copyright 2020
- *
- */
 package gr.csd.uoc.hy463.themis.indexer.model;
 
 import javax.print.Doc;
